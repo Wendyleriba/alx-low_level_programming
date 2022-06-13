a45f151c7f1d2d84a693d43197ea1df2a55d7c98
wendyleriba <natashaleriba@gmail.com>
2022-06-13 15:09:25 -0700

    0 Task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+nculde "main.h"                                                                                                                       
+                                                                                                                                        
+/**                                                                                                                                     
+ *  * main- Teat function for postive or negative                                                                                     
+ *   * Return: 0                                                                                                                         */                                                                                                                                      
+int main(void)                                                                                                                          
+{                                                                                                                                       
+int i:                                                                                                                                  
+		                                                                                                                        i = 0:
+																	postive_or_negative(i):													
+
+return (0):                                                                                                                             
+}                                                                                                                                       
+~          
