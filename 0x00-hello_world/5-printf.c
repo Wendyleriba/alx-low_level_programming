a51202e14fe51271b48a49c738dcd4715b1b63a4
wendyleriba <natashaleriba@gmail.com>
2022-06-13 16:11:26 -0700

    4-puts.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
-#inculde < stdio.h >
+#include <stdio.h>
 
+/**
+ * main-prints the size of various types;
+ * Return: 0 if exited properly, non-zero otherwise;
+ */ 
 int main(void)
 {
-prinf("with proper grammar, but the outcome is a piece of art,");
+prinf("\"with proper grammar, but the outcome is a piece of art");
 return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-
 /**
  *  * main - Entry point
  *   *
  *    * Return: Always 0 (Success)
- */
+ *     */
 int main(void)
 {
-puts(const char "\" Programming is like building a multilingual puzzle");
-return (0);
+printf("Size of char: %d byte(s)\n, 
+	    return (0);
 }
