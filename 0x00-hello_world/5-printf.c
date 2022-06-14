aee3c6791c3ae8a0148a7207cb47db6104af5318
wendyleriba <natashaleriba@gmail.com>
2022-06-13 17:30:48 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main-prints the size of various types;
+ * main- prints the size of various types;
  * Return: 0 if exited properly, non-zero otherwise;
- */ 
+ */
+
 int main(void)
 {
-prinf("\"with proper grammar, but the outcome is a piece of art");
+printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
