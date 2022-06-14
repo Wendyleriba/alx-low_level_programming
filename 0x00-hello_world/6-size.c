e43606a45ea3a2fb5c67158dcc2ee0c6b61323e1
wendyleriba <natashaleriba@gmail.com>
2022-06-14 13:27:55 -0700

    5-printf.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
+#include <stdio.h>                                                                                                            
 
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+/**                                                                                                                                   *  * main- prints the size of various types;                                                                                          *   * Return: 0 if exited properly, non-zero otherwise;                                                                               */
+
 int main(void)
 {
-printf("Size of char: %d byte(s)\n, 
-	    return (0);
+printf("Size of char : % d byte(s)");
+printf("Size of an int : % d byte(s)");
+printf("Size of a long int : % d byte(s)");
+printf("Size of a long long int : % d byte(s)");
+printf("Size of a float : % d byte(s)");
+return (0);
 }
